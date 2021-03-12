39f5fd173206944590c8fd10ab3b954aba96ed1b
deesrew <deesrew@yandex.ru>
2021-03-12 23:45:37 +0300

    task 1

---
+++ FibonacciNumbers.php
@@ -0,0 +1,64 @@
+<?php
+
+class FibonacciNumbers
+{
+    private array $numbers = array();
+    private $lastNumber;
+
+    public function __construct()
+    {
+        $this->setNumbers();
+    }
+
+    /**
+     * @return string|int
+     */
+    public function getLastNumber()
+    {
+        return $this->lastNumber;
+    }
+
+    /**
+     * @param string|int $lastNumber
+     * @return void
+     */
+    public function setLastNumber($lastNumber): void
+    {
+        $this->lastNumber = $lastNumber - 1;
+    }
+
+    /**
+     * @return void
+     */
+    public function setNumbers(): void
+    {
+        $this->numbers = array(1, 1);
+    }
+
+    /**
+     * @return array
+     */
+    public function getNumbers(): array
+    {
+        return $this->numbers;
+    }
+
+    /**
+     * @return array
+     * @var int|string $count
+     */
+    public function setFirstNumbers(): array
+    {
+        for ($i = 1; $i < $this->lastNumber; ++$i) {
+            $this->numbers[] = $this->numbers[$i] + $this->numbers[$i - 1];
+        }
+        return $this->numbers;
+    }
+
+}
+
+$obj = new FibonacciNumbers();
+$obj->setLastNumber(64);
+$number = $obj->setFirstNumbers();
+print_r($number);
+
